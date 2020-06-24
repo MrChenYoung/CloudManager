f582b2f817fda86ffb1bbc0c4e7e6c118a6884a8
code007 <yi_aohehe@163.com>
2020-06-25 03:26:39 +0800

    new

--- admin/controller/API/API_BaseController.class.php
+++ admin/controller/API/API_BaseController.class.php
@@ -76,6 +76,9 @@ class API_BaseController extends Controller
         $fileSize = "--";
         $fileCount = 0;
         $getSizeCmd = "rclone size ".$remoteName.":".$path;
+        echo $getSizeCmd;
+        die;
+        
         $sizeRes = ShellManager::exec($getSizeCmd);
         if (!$sizeRes["success"]){
             // 获取文件详情失败
