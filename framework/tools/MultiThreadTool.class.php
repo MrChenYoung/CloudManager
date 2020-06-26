eb65e11a4519b09b33f55e51f4dac81259e1e464
code007 <yi_aohehe@163.com>
2020-06-26 11:33:18 +0800

    new

--- framework/tools/MultiThreadTool.class.php
+++ framework/tools/MultiThreadTool.class.php
@@ -10,7 +10,10 @@ class MultiThreadTool
         $param["API"] = "";
         $param["action"] = $action;
         self::doRequest($url, $param);
-        // 为了使脚本完整执行，不会中断执行，需要在nginx.conf添加fastcgi_intercept_errors on;配置，并设置以下两项
+        // 为了使脚本完整执行，不会中断执行，需要配置：
+        //1. 在nginx.conf添加fastcgi_intercept_errors on;配置
+        //2. 设置php.ini里max_execution_time值为更大(默认30,表示脚本最长执行时长为30s)，或者设为0，表示时长不限
+        //3. 设置以下两行代码
         ignore_user_abort(true); // 忽略客户端断开
         set_time_limit(0);    // 设置执行不超时
     }
