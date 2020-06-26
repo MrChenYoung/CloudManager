94d145958da661099e582a14b980d9f59c324b99
code007 <yi_aohehe@163.com>
2020-06-26 21:15:41 +0800

    new

--- admin/controller/API/API_FileTransferController.class.php
+++ admin/controller/API/API_FileTransferController.class.php
@@ -42,10 +42,7 @@ class API_FileTransferController extends API_BaseController
             "sourceId"=>$end,
             "savePath"=>$path
         ];
-
-        echo "<pre>";
-        var_dump($params);
-        die;
+        
         MultiThreadTool::addTask($this->website."/index.php","fileTransfer",$params);
         // 提示正在后台更新
         echo $this->success("文件后台转存中");
