e1d0e5a4359cc9507fe2943b6a38a5d4ffe4cfd3
code007 <yi_aohehe@163.com>
2020-06-27 20:06:18 +0800

    new

--- admin/controller/AsynTaskController.class.php
+++ admin/controller/AsynTaskController.class.php
@@ -126,6 +126,7 @@ class AsynTaskController extends Controller
         if (!$res["success"]){
             file_put_contents($filePath,"文件转存失败");
         }
+        file_put_contents($filePath,"\r\n 转存完成");
         // 转存完成 修改数据库标识
         DatabaseDataManager::getSingleton()->update("file_transfer_info",["status"=>0],["id"=>1]);
     }
