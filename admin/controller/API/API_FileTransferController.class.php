031eb3280f3f41d407e88a87e56f5c2f5b89683e
code007 <yi_aohehe@163.com>
2020-06-28 16:59:20 +0800

    new

--- admin/controller/API/API_FileTransferController.class.php
+++ admin/controller/API/API_FileTransferController.class.php
@@ -60,20 +60,6 @@ class API_FileTransferController extends API_BaseController
         echo $this->success("文件后台转存中");
     }
 
-    // 转存日志文件是否存在
-    public function fileTransferExist(){
-        $data = $this->getFileTransferStatus();
-        $fileName = "";
-        if (is_array($data) && key_exists("file_name",$data)){
-            $fileName = $data["file_name"];
-        }
-        if (file_exists(LogManager::getSingleton()->logFilePath)){
-            echo $this->success(["flag"=>true,"content"=>$fileName]);
-        }else {
-            echo $this->success(["flag"=>false,"content"=>"没有转存任务"]);
-        }
-    }
-
     // 是否正在转存文件
     public function fileTransfering(){
         $data = $this->getFileTransferStatus();
--- admin/controller/API/API_SettingController.class.php
+++ admin/controller/API/API_SettingController.class.php
@@ -3,11 +3,7 @@
 
 namespace admin\controller\API;
 
-
-use admin\controller\AsynTaskController;
 use framework\tools\DatabaseDataManager;
-use framework\tools\MultiThreadTool;
-use framework\tools\ShellManager;
 
 class API_SettingController extends API_BaseController
 {
@@ -41,89 +37,6 @@ class API_SettingController extends API_BaseController
             echo $this->success($res);
         }else {
             echo $this->failed("更改设置失败");
-        }
-    }
-
-    // 获取云盘文件夹缓存
-    public function loadDriveDirCache(){
-        // 远程云盘名
-        if (!isset($_GET["remoteName"])){
-            echo $this->failed("缺少remoteName参数");
-            die;
-        }
-        $remoteName = $_GET["remoteName"];
-
-        $path = ADMIN."resource/driveDirTreeCache/".$remoteName.".json";
-        $data = [];
-        if (file_exists($path)){
-            // 有缓存
-            $str = file_get_contents($path);
-            $data = json_decode($str);
-        }
-
-        // 获取目录树更新状态
-        $res = DatabaseDataManager::getSingleton()->find("driver_setting",["flag"=>"updatingDirTree"]);
-        $status = 0;
-        if ($res){
-            $status = (int)$res[0]["status"];
-        }
-        echo $this->success(["status"=>$status,"data"=>$data]);
-    }
-
-    // 更新云盘目录树缓存
-    public function updateDriveDirList(){
-        // 远程云盘名
-        if (!isset($_GET["remoteName"])){
-            echo $this->failed("缺少remoteName参数");
-            die;
-        }
-        $remoteName = $_GET["remoteName"];
-
-        // 因为更新云盘目录树需要很长时间，所以使用异步执行
-        // 添加异步任务
-        $params = [
-            "m"=>"admin",
-            "c"=>"AsynTask",
-            "a"=>"index",
-            "name"=>$remoteName
-        ];
-        MultiThreadTool::addTask($this->website."/index.php","updateDriveDirList",$params);
-        // 提示正在后台更新
-        echo $this->success("目录树后台更新中");
-    }
-
-    // 一键更新所有云盘目录树缓存
-    public function updataAllDriverDirCache(){
-        // 获取云盘列表
-        $driverList = (new API_FileManagerController())->loadDriverList(true);
-        $drivers = [];
-        foreach ($driverList as $ds) {
-            foreach ($ds as $d) {
-                $drivers[] = $d;
-            }
-
-        }
-        $drivers = implode(",",$drivers);
-
-        // 后台更新
-        $params = [
-            "m"=>"admin",
-            "c"=>"AsynTask",
-            "a"=>"index",
-            "drivers"=>$drivers
-        ];
-        MultiThreadTool::addTask($this->website."/index.php","updateDriveDirList",$params);
-        // 提示正在后台更新
-        echo $this->success("目录树后台更新中");
-    }
-
-    // 复位目录树更新标志位
-    public function resetFlag() {
-        $res = DatabaseDataManager::getSingleton()->update("driver_setting",["status"=>0],["flag"=>"updatingDirTree"]);
-        if ($res){
-            echo $this->success("");
-        }else {
-            echo $this->failed("");
         }
     }
 
