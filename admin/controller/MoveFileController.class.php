dc83aa6f5c12044e5bc667164e50df78c56678d3
code007 <yi_aohehe@163.com>
2020-06-29 19:17:12 +0800

    new

--- admin/controller/MoveFileController.class.php
+++ admin/controller/MoveFileController.class.php
@@ -1,4 +1,22 @@
 <?php
 
-$path = $argv[1];
-file_put_contents($path,"测试移动文件，参数1:".$argv[1]." 参数2:".$argv[2],FILE_APPEND);
+// 日志文件路径
+$logPath = $argv[1];
+// mysql数据库信息
+$mysqlOption = json_decode($argv[2]);
+// 链接数据库
+$mysqli = new \mysqli($mysqlOption['host'],$mysqlOption['user'],$mysqlOption['pass'],$mysqlOption['dbname'],$mysqlOption['port']);
+
+if ($mysqli -> connect_error){
+    addLog($logPath,"数据库连接失败：".$mysqli -> connect_error);
+}
+
+// 添加log
+function addLog($path,$content){
+    // 获取当前时间
+    date_default_timezone_set('PRC');
+    $time = date('Y-m-d H:i:s', time());
+    $content = "[$time]".$content."\r\n";
+    file_put_contents($path,$content,FILE_APPEND);
+}
+
