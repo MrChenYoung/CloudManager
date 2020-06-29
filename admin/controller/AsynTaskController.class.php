4921dcee3ece25b8e12cf9c0ac02c44e1444ac4a
code007 <yi_aohehe@163.com>
2020-06-29 19:44:43 +0800

    new

--- admin/controller/AsynTaskController.class.php
+++ admin/controller/AsynTaskController.class.php
@@ -66,41 +66,18 @@ class AsynTaskController extends Controller
         if (!isset($_REQUEST["desPath"])) die;
         $desPath = $_REQUEST["desPath"];
 
+        // 添加日志
+        LogManager::getSingleton()->clearLog();
+        LogManager::getSingleton()->addLog("移动".$sourcePath."到".$desPath);
+
+        // 执行移动文件php脚本
         $cmd = "php ".ADMIN."controller/MoveFileController.class.php ".LogManager::getSingleton()->logFilePath." '".json_encode($GLOBALS["db_info"])."'";
-        LogManager::getSingleton()->addLog("cmd:".$cmd);
+        $cmd = $cmd." ".$sourcedriver." ".$sourcePath." ".$desdriver." ".$desPath;
         $res = ShellManager::exec($cmd);
         if (!$res["success"]){
             LogManager::getSingleton()->addLog("执行测试php脚本失败:".json_encode($res));
         }else {
             LogManager::getSingleton()->addLog("执行测试php脚本成功:".json_encode($res));
         }
-
-        // 添加到数据库记录
-//        DatabaseDataManager::getSingleton()->insert("file_move_info",["source_path"=>$sourcePath,"des_path"=>$desPath]);
-//
-//        // 添加日志
-//        LogManager::getSingleton()->clearLog();
-//        LogManager::getSingleton()->addLog("移动".$sourcePath."到".$desPath);
-//        if ($sourcedriver == $desdriver){
-//            $cmd = "rclone moveto ".$sourcePath." ".$desPath." --drive-server-side-across-configs -P >> ".LogManager::getSingleton()->logFilePath." 2>&1";
-//        }else {
-//            $cmd = "rclone moveto ".$sourcePath." ".$desPath." -P >> ".LogManager::getSingleton()->logFilePath." 2>&1";
-//        }
-//
-//        LogManager::getSingleton()->addLog("命令:".$cmd);
-//        $res = ShellManager::exec($cmd);
-//        if (!$res["success"]){
-//            // 移动失败
-//            LogManager::getSingleton()->addLog("文件移动失败");
-//        }else {
-//            // 移动成功
-//            // 如果是跨云盘移动文件夹，原来的云盘里的文件夹会变成空，但是文件夹依然存在，需要再执行删除空文件夹命令
-//            if ($sourcedriver != $desdriver){
-//                ShellManager::exec("rclone purge ".$sourcePath);
-//            }
-//            LogManager::getSingleton()->addLog("文件移动成功");
-//        }
-//        // 从数据库记录删除
-//        DatabaseDataManager::getSingleton()->delete("file_move_info");
     }
 }
--- admin/controller/MoveFileController.class.php
+++ admin/controller/MoveFileController.class.php
@@ -6,11 +6,53 @@ $logPath = $argv[1];
 $mysqlOption = json_decode($argv[2],true);
 // 链接数据库
 $mysqli = new \mysqli($mysqlOption['host'],$mysqlOption['user'],$mysqlOption['pass'],$mysqlOption['dbname'],$mysqlOption['port']);
-
 if ($mysqli -> connect_error){
     addLog($logPath,"数据库连接失败：".$mysqli -> connect_error);
+}
+
+// 源云盘
+$sourcedriver = $argv[3];
+// 源路径
+$sourcePath =  $argv[4];
+// 目标云盘
+$desdriver = $argv[5];
+// 目标路劲
+$desPath = $argv[6];
+
+// 添加到数据库记录
+insert($mysqli,["source_path"=>$sourcePath,"des_path"=>$desPath]);
+
+if ($sourcedriver == $desdriver){
+    $cmd = "rclone moveto ".$sourcePath." ".$desPath." --drive-server-side-across-configs -P >> ".$logPath." 2>&1";
 }else {
-    addLog($logPath,"数据库链接成功");
+    $cmd = "rclone moveto ".$sourcePath." ".$desPath." -P >> ".$logPath." 2>&1";
+}
+
+// 执行rclone移动移动命令
+addLog($logPath,"命令:".$cmd);
+$res = myshellExec($cmd);
+if (!$res["success"]){
+    // 移动失败
+    addLog("文件移动失败");
+}else {
+    // 移动成功
+    // 如果是跨云盘移动文件夹，原来的云盘里的文件夹会变成空，但是文件夹依然存在，需要再执行删除空文件夹命令
+    if ($sourcedriver != $desdriver){
+        myshellExec("rclone purge ".$sourcePath);
+    }
+    addLog("文件移动成功");
+}
+// 从数据库记录删除
+delete($mysqli);
+
+// 执行shell脚本
+function myshellExec($mycmd){
+    exec($mycmd.' 2>&1',$result,$status);
+    $success = $status == 0 ? true : false;
+    return [
+        "success"   =>  $success,
+        "result"    =>  $result
+    ];
 }
 
 // 添加log
@@ -22,3 +64,68 @@ function addLog($path,$content){
     file_put_contents($path,$content,FILE_APPEND);
 }
 
+// 插入数据到数据库
+function insert($mysqlDAO,$data){
+    $sql = "INSERT INTO file_move_info ";
+
+    //2. 将传递的数组中的字段名拼接成字符串
+    $keys = [];
+    $values = [];
+    foreach($data as $k=>$v){
+        $keys[] = "`$k`";                    // `goods_name`    `shop_price`
+        $values[] = $v;                     // '小米Mix2'  '1 or 1=1 \''
+    }
+    //3. 拼接字段部分
+    $fields = '('.implode(',',$keys).')';    // (`goods_name`,`shop_price`)
+    $sql .= $fields;
+
+    //3. 拼接字段值部分
+    $fileds = ' VALUES('.implode(',',$values).')';
+    $sql .= $fileds;
+
+    //执行sql语句
+    $result = $mysqlDAO -> query($sql);
+    if ($result){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+// 删除数据库记录
+function delete($mysqlDAO,$where=[]){
+    $index = 0;
+    $whereStr = "";
+    foreach ($where as $key => $value) {
+        $value = singleQuotesInclude($value);
+        if ($index > 0){
+            $whereStr .= " AND `$key`=$value";
+        }else {
+            $whereStr .= " `$key`=$value";
+        }
+        $index++;
+    }
+
+    if (strlen($whereStr) > 0){
+        $sql = "DELETE FROM file_move_info WHERE $whereStr";
+    }else {
+        $sql = "DELETE FROM file_move_info";
+    }
+
+    $res = $mysqlDAO -> query($sql);
+    if ($res){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+function singleQuotesInclude($str){
+    $firstChar = substr($str,0,1);
+    $lastChar = substr($str, strlen($str) - 1,1);
+
+    $str = $firstChar === "'" ? $str : "'".$str;
+    $str = $lastChar === "'" ? $str : $str."'";
+
+    return $str;
+}
