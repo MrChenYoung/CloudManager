ba0b90d9740e4b5999b22c6d4f9fdc4d051065f8
code007 <yi_aohehe@163.com>
2020-07-01 00:12:46 +0800

    new

--- admin/controller/CreateTablesController.class.php
+++ admin/controller/CreateTablesController.class.php
@@ -50,9 +50,9 @@ class CreateTablesController
                         id int AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
                         driver_name varchar(64) DEFAULT '' COMMENT '云盘名',
                         main_admin varchar(64) DEFAULT '' COMMENT '主管理员',
-                        member_count varchar(64) DEFAULT '' COMMENT '成员数量'
+                        member_count varchar(64) DEFAULT '' COMMENT '成员数量',
                         remark varchar(300) DEFAULT '' COMMENT '备注'
-                    ) DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='云盘表';
+                    ) DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='云盘信息表';
 EEE;
         $this->dao->createTable($tableName,$sql);
     }
