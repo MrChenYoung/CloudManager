f66d1331d38dc4a43d124bd5f4f49e5ebb8b8ed9
code007 <yi_aohehe@163.com>
2020-07-02 18:10:05 +0800

    new

--- admin/controller/API/API_FileManagerController.class.php
+++ admin/controller/API/API_FileManagerController.class.php
@@ -420,20 +420,12 @@ class API_FileManagerController extends API_BaseController
 
         // 转义空格
         $sourcePath = str_replace(" ","\ ",$sourcePath);
-        // 获取要移动文件的大小
-        $res = $this->loadDetaileInfo("","",$sourcePath);
-        $size = (int)$res["sizeBytes"];
-        //  如果要移动的文件大于10G，转入后台移动
-        if ($size > 10 * 1024 * 1024 * 1024){
-            // 查看是否有文件正在后台移动
-            $tableRes = DatabaseDataManager::getSingleton()->find("file_move_info");
-            if ($tableRes && count($tableRes) > 0){
-                echo $this->success(["canMove"=>false,"back"=>true,"msg"=>"有文件正在后台移动中"]);
-            }else {
-                echo $this->success(["canMove"=>true,"back"=>true,"msg"=>""]);
-            }
+        // 查看是否有文件正在后台移动
+        $tableRes = DatabaseDataManager::getSingleton()->find("file_move_info");
+        if ($tableRes && count($tableRes) > 0){
+            echo $this->success(["canMove"=>false,"msg"=>"有文件正在后台移动中"]);
         }else {
-            echo $this->success(["canMove"=>true,"back"=>false,"msg"=>""]);
+            echo $this->success(["canMove"=>true,"msg"=>""]);
         }
     }
 
