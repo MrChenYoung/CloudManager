224c38b71c50ff26e1c4799033c5e2fb95127638
code007 <yi_aohehe@163.com>
2020-07-02 21:45:50 +0800

    new

---
+++ admin/controller/API/API_FileSyncController.class.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace admin\controller\API;
+
+
+use framework\tools\DatabaseDataManager;
+
+class API_FileSyncController extends API_BaseController
+{
+    // 表名
+    private $tableName;
+    public function __construct()
+    {
+        parent::__construct();
+        $this->tableName = "file_sysnc_info";
+    }
+
+    // 获取同步列表
+    public function loadFileSyncList(){
+        $res = DatabaseDataManager::getSingleton()->find($this->tableName);
+
+        $data = [];
+        if ($res){
+            foreach ($res as $re) {
+                $type = $re["type"] == 1 ? "本地同步" : "线上同步";
+                $status = $re["status"] == 1 ? "同步中" : "未同步";
+                $re["typeStr"] = $type;
+                $re["statusStr"] = $status;
+                $data[] = $re;
+            }
+        }
+        echo $this->success($data);
+    }
+}
