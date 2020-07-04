4ed78468f55075fc48a71902c57b373e49fb4fde
code007 <yi_aohehe@163.com>
2020-07-04 14:37:33 +0800

    new

--- admin/controller/API/API_FileSyncController.class.php
+++ admin/controller/API/API_FileSyncController.class.php
@@ -5,6 +5,7 @@ namespace admin\controller\API;
 
 
 use framework\tools\DatabaseDataManager;
+use framework\tools\MultiThreadTool;
 
 class API_FileSyncController extends API_BaseController
 {
@@ -35,6 +36,61 @@ class API_FileSyncController extends API_BaseController
 
     // 添加/修改同步信息
     public function updateSyncInfo(){
+        // 同步类型
+        if (!isset($_GET["type"])){
+            echo $this->failed("缺少type参数");
+            die;
+        }
+        $type = $_GET["type"];
 
+        // 源云盘
+        if (!isset($_GET["srcDrive"])){
+            echo $this->failed("缺少srcDrive参数");
+            die;
+        }
+        $srcDrive = $_GET["srcDrive"];
+
+        // 目标云盘
+        if (!isset($_GET["desDrive"])){
+            echo $this->failed("缺少desDrive参数");
+            die;
+        }
+        $desDrive = $_GET["desDrive"];
+
+        // 源路径
+        if (!isset($_GET["srcPath"])){
+            echo $this->failed("缺少srcPath参数");
+            die;
+        }
+        $srcPath = $_GET["srcPath"];
+        $srcPath = base64_decode($srcPath);
+        $srcPath = urldecode($srcPath);
+        if ($type == "0"){
+            $srcPath = $srcDrive.":".$srcPath;
+        }
+
+        // 目标路径
+        if (!isset($_GET["desPath"])){
+            echo $this->failed("缺少desPath参数");
+            die;
+        }
+        $desPath = $_GET["desPath"];
+        $desPath = base64_decode($desPath);
+        $desPath = urldecode($desPath);
+        if ($type == "0"){
+            $desPath = $desDrive.":".$desPath;
+        }
+
+        $data = [
+            "source_path"   =>$srcPath,
+            "des_path"      =>$desPath,
+            "type"          =>$type
+        ];
+        $res = DatabaseDataManager::getSingleton()->insert($this->tableName,$data);
+        if ($res){
+            echo $this->success("添加同步成功");
+        }else {
+            echo $this->failed("添加同步失败");
+        }
     }
 }
